bcaf4167ec3c44c6fc7daab8cccf0d8067257de4
Rcdy2 <roccacortezdayanna@gmail.com>
2026-03-29 13:53:22 -0500

    Update company configuration details in empresa.py

--- config/empresa.py
+++ config/empresa.py
@@ -1,8 +1,9 @@
-NOMBRE_EMPRESA = "Soluciones Financieras Profesionales S.A.S."
-DEPARTAMENTO = "Departamento de Cobranzas"
-TELEFONO = "(01) 123 4567"
-EMAIL_CONTACTO = "cobranza@miempresa.com"
+NOMBRE_EMPRESA = "Colocar nombre de la empresa correspondiente"
+DEPARTAMENTO = "Area de la empres aque envia los correos"
+TELEFONO = "(01) 00000000"
+EMAIL_CONTACTO = "correo@miempresa.com"
 DIRECCION = "Calle 123 #45-67, Lima, Perú"
 URL_PAGOS = "www.miempresa.com/pagos"
 EMAIL_COMPROBANTES = "pagos@miempresa.com"
+
 ICONO_EMPRESA = "icon/empresa1SF.png"
